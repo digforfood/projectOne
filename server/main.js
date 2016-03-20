f462aac336507ebff9a10109e6a973767674082d
digforfood <pgeekadmin@gmail.com>
2016-03-20 21:44:47 +0300

    hjkihj

--- server/main.js
+++ server/main.js
@@ -3,7 +3,24 @@ var WebSocketServer = new require('ws'),
 	webSocketServer = new WebSocketServer.Server({port:443}),
 	currFrameTick = new Date(),
 	prevFrameTick,
-	deltaFrameTick;
+	deltaFrameTick,
+
+	playerId,
+	players;
+
+
+////////////////////_DB_////////////////////
+var db = {'users':[
+		{
+			'login': '',
+			'pass': ''
+		},
+		{
+			'login': '',
+			'pass': ''
+		}
+	]};
+////////////////////_DB_////////////////////
 
 
 /*
@@ -26,6 +43,26 @@ main
 ==============
 */
 function main(){
+	playerId = 0;
+	players = {};
+
+	webSocketServer.on('connection', function(client){
+		var id = playerId++;
+		var player = {};
+		player.socket = client;
+		players[id] = player;
+
+		player.socket.on('message', function(message){
+			console.log(message);
+			var msg = {'m': [{'t': 2, 'd': {'k': 112233, 's': 2}}]};
+			this.send(JSON.stringify(msg), function(){ /* ignore errors */ });
+		});
+
+		player.socket.on('close', function(){
+			player.quit = true;
+		});
+	});
+
 	gameWorldLoop();
 }
 
